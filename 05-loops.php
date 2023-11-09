e7193dc04878e28c79b53583f3872224c76c0a6b
YtecSolutions <142538265+YtecSolutions@users.noreply.github.com>
2023-11-09 13:59:54 -0300

    exercicio04

---
+++ 05-loops.php
@@ -0,0 +1,102 @@
+<?php
+require "recursos.php";
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loops</title>
+<style>
+    ol li:nth-child(odd) { color: blue; }
+    ol li:nth-child(even) { color: red; }
+</style>
+</head>
+<body>
+ <?php foreach($linguagens as $linguagem){?>
+    <p><i><?=$linguagem?></i></p>
+<?php 
+ } ?>
+
+<h1>Loops</h1>
+<hr>
+<h2>Tradicionais: for, while, do/while</h2>
+
+<h3>while</h3>
+<?php
+$i = 1;
+while($i <= 3){
+?>
+    <p>Senac Penha - I vale <?=$i?> </p>
+<?php
+    $i++;
+}
+?>
+
+<h3>do/while</h3>
+<ul>
+
+<?php 
+$j = 1;
+do {
+?>
+    <li>Item: <?=$j?></li>
+<?php 
+    $j++;
+} while($j <= 5)
+?>
+
+</ul>
+
+<h3>for</h3>
+<?php
+$alunos = ["Jean", "Giuseppe", "Arthur", "Pedro", "Paola"];
+
+$quantidade = count($alunos);
+
+for( $i = 0; $i < $quantidade; $i++ ){
+?>
+    <p> Aluno: <?=$alunos[$i]?> </p>
+<?php
+}
+?>
+
+<h3>foreach</h3>
+
+<?php foreach( $alunos as $aluno ){ ?>
+    <p> <?=$aluno?> </p>
+<?php } ?>
+
+<h2>Exercício 03</h2>
+
+<p>1) Crie um array contendo o nome dos
+12 meses do ano.</p>
+
+<p>2) Monte uma <b>lista ordenada</b> exibindo
+o nome de cada mês em um item.</p>
+
+<p><b>DESAFIO:</b> <i>formate a lista
+de uma forma que os meses apareçam com
+cores alternadas. Exemplo: janeiro/março em
+azul, fevereiro/abril em vermelho etc.
+</i></p>
+
+<?php
+$meses = [
+    "janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"
+];
+?>
+
+<ol>
+<?php foreach($meses as $mes){ ?>
+    <li> <?=$mes?> </li>
+<?php } ?>
+</ol>
+
+
+
+
+</body>
+</html>
---
+++ exercicio04-dados.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Exercício 04 (Dados)</title>
+</head>
+<body>
+<h1>Exercício 04 (dados/processamento)</h1>
+<hr>
+<?php 
+$produto = $_POST["produto"];
+$fabricante = $_POST["fabricante"];
+$preco = $_POST["preco"];
+$descricao = $_POST["descricao"];
+?>
+    <ul>
+<li>produto:<?=$produto?></li>
+
+<li>fabricante: <?=$fabricante?></li>
+<!-- A virgua é o tipo de caracter para separar os numeros (NESTE CASO) -->
+<li>preco <?=number_format($preco, 2, ',', '.')?></li> 
+
+<li>descricao: <?=$descricao?></li>
+
+</ul>
+
+
+
+
+</body>
+</html>
